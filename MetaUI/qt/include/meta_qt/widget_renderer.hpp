51f9054adbc8c13b830199adebf383275478ebbb
Otto Link <otto.link.bv@gmail.com>
2026-06-26 13:28:05 +0200

    Code format

--- MetaUI/qt/include/meta_qt/widget_renderer.hpp
+++ MetaUI/qt/include/meta_qt/widget_renderer.hpp
@@ -55,6 +55,5 @@ MetaWidget *render(AbstractAttribute *p_attr, QWidget *parent = nullptr);
 
 #ifdef META_ENABLE_GLM_TYPES
 #include "meta_qt/widget_renderer_inl/glm_ivec2.inl"
-
 #include "meta_qt/widget_renderer_inl/glm_vec2.inl"
 #endif
